a6abf15c02d3f70a9d7641f2cd2c9aa008c9145a
Rendall <rendall@gmail.com>
2026-02-05 22:22:03 +0200

    Use Ranvire org for bundle installs

--- test/install-bundle.test.js
+++ test/install-bundle.test.js
@@ -8,32 +8,32 @@ const {
 } = require('../util/install-bundle');
 
 describe('install-bundle helpers', function() {
-  it('resolves bare bundle names to the Ranvier org', function() {
+  it('resolves bare bundle names to the Ranvire org', function() {
     const resolved = resolveBundleRemote('bundle-example-areas');
     assert.strictEqual(
       resolved,
-      'https://github.com/Ranvier/bundle-example-areas'
+      'https://github.com/Ranvire/bundle-example-areas'
     );
   });
 
   it('resolves org/repo shorthand to the GitHub URL', function() {
-    const resolved = resolveBundleRemote('Ranvier/bundle-example-areas');
+    const resolved = resolveBundleRemote('Ranvire/bundle-example-areas');
     assert.strictEqual(
       resolved,
-      'https://github.com/Ranvier/bundle-example-areas'
+      'https://github.com/Ranvire/bundle-example-areas'
     );
   });
 
   it('keeps explicit remotes untouched', function() {
-    const httpsRemote = 'https://github.com/Ranvier/bundle-example-areas';
+    const httpsRemote = 'https://github.com/Ranvire/bundle-example-areas';
     assert.strictEqual(resolveBundleRemote(httpsRemote), httpsRemote);
 
-    const sshRemote = 'git@github.com:Ranvier/bundle-example-areas.git';
+    const sshRemote = 'git@github.com:Ranvire/bundle-example-areas.git';
     assert.strictEqual(resolveBundleRemote(sshRemote), sshRemote);
   });
 
   it('extracts the bundle name from a resolved remote', function() {
-    const name = getBundleName('https://github.com/Ranvier/bundle-example-areas');
+    const name = getBundleName('https://github.com/Ranvire/bundle-example-areas');
     assert.strictEqual(name, 'bundle-example-areas');
   });
 });
